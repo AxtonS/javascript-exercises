7b89cec88f2ff74079258a2112f8267d3a1c800d
Axton Smith <axtonsmith@gmail.com>
2022-05-26 22:23:57 -0400

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+    let sum = 0;
+    if (((numOne || numTwo) >= 0) && (typeof(numOne) && (typeof(numTwo) === 'number'))) {
+        if (numOne > numTwo) {
+            for (i = numOne; i >= numTwo; i--) {
+                sum += i;
+            }
+        }
+        if (numOne < numTwo) {
+            for (i = numOne; i <= numTwo; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
