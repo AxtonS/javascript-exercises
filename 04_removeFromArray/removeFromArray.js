7996e7ce543cc6c097ad07dec05cefae1ccb855f
Axton Smith <axtonsmith@gmail.com>
2022-05-26 22:02:45 -0400

    Finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
-
+    let length = arguments.length;          
+    let args = Array.from(arguments);       
+    let array = args[0];                    
+    for(i = length - 1; i > 0; i--){        
+        let arg = args[i];                  
+        for(j = array.length -1; j >=0; j--){
+            if(array[j] === arg){             
+                array.splice(j, 1);         
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
