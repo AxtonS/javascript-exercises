3e219464c459c6b5f8cb47cc89cfb89e5cb1927b
Axton Smith <axtonsmith@gmail.com>
2022-05-31 21:19:16 -0400

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let b = 1;
+  if (a === 0 || a === 1){
+    return b;
+  } else {
+    for(i = a; i >= 1; i--){
+      b *= i;
+    }
+    return b
+  }
 };
 
 // Do not edit below this line
