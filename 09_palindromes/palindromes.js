8b72a5f53f514b4ddeb52a59f1c6fe1444497dfe
Axton Smith <axtonsmith@gmail.com>
2022-05-31 21:37:50 -0400

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newString = str.toLowerCase().replace(/[^a-z]/g, '');
+    return (
+        newString.split('').reverse().join('') == newString
+    );
 };
 
 // Do not edit below this line
